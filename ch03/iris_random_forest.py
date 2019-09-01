cd6e26b290105a9eef074b43399000f9abb307bb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-09-01 21:19:38 +0900

    ch03: random forest

---
+++ ch03/iris_random_forest.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+from decision_region import plot_decision_regions
+from iris_split import X_train, y_train, X_test, y_test
+
+forest = RandomForestClassifier(criterion='gini',
+                                n_estimators=25, 
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+# 2019.09.01 add
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/03_22.png', dpi=300)
+plt.show()
