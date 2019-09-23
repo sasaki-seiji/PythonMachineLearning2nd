6cfdcba61b1107ffa783ee45c9afcefb106f234d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-09-23 18:01:10 +0900

    ch08: imdb logistic regression classifier

---
+++ ch08/imdb_gs_lr_tfidf.py
@@ -0,0 +1,61 @@
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+
+from imdb_cleansing import df
+from tokenizer import tokenizer, tokenizer_porter
+from stop_word import stop
+
+# # Training a logistic regression model for document classification
+
+# Strip HTML and punctuation to speed up the GridSearch later:
+
+
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+
+
+
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf':[False],
+               'vect__norm':[None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=1,
+                           n_jobs=-1)
+
+gs_lr_tfidf.fit(X_train, y_train)
+
+
+
+# 2019.09.23 change
+if __name__ == '__main__':
+    print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+    print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+    clf = gs_lr_tfidf.best_estimator_
+    print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
