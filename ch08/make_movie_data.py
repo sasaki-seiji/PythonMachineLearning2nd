e0b484571ed10025db22402c9359f004ff2051cc
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-02 15:58:04 +0900

    miss saving

--- ch08/make_movie_data.py
+++ ch08/make_movie_data.py
@@ -43,10 +43,3 @@ df = df.reindex(np.random.permutation(df.index))
 
 
 df.to_csv('movie_data.csv', index=False, encoding='utf-8')
-
-
-
-
-
-df = pd.read_csv('movie_data.csv', encoding='utf-8')
-df.head(3)
