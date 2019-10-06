4f9130ed29787dc62c69278b742be09b7600673f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-06 12:10:27 +0900

    ch09: review db test

---
+++ ch09/movieclassifier/review_db_query_test.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('reviews.sqlite')
+c = conn.cursor()
+c.execute("SELECT * FROM review_db WHERE date"\
+    " BETWEEN '2017-01-01 00:00:00' AND DATETIME('now')")
+results = c.fetchall()
+conn.close()
+print(results)    
