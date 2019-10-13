0fc9a14c666f106f130ea23b23292b630fca2901
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-13 17:12:52 +0900

    ch10: pandas for housing data set

---
+++ ch10/housing_pd.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
+                 'python-machine-learning-book-2nd-edition'
+                 '/master/code/ch10/housing.data.txt',
+                 header=None,
+                 sep='\s+')
+
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+df.head()
+# 2019.10.13 add
+if __name__ == '__main__':
+    print(df.head())
