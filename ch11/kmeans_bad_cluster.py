d0a1a0666bcdfb48404e1821ccbb1fb6a7293174
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-22 09:51:59 +0900

    ch11: silhouette analysis

---
+++ ch11/kmeans_bad_cluster.py
@@ -0,0 +1,45 @@
+# Comparison to "bad" clustering:
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# 2019.10.22 add
+X, y = make_blobs(n_samples=150, 
+                  n_features=2, 
+                  centers=3, 
+                  cluster_std=0.5, 
+                  shuffle=True, 
+                  random_state=0)
+
+km = KMeans(n_clusters=2,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+y_km = km.fit_predict(X)
+
+plt.scatter(X[y_km == 0, 0],
+            X[y_km == 0, 1],
+            s=50,
+            c='lightgreen',
+            edgecolor='black',
+            marker='s',
+            label='cluster 1')
+plt.scatter(X[y_km == 1, 0],
+            X[y_km == 1, 1],
+            s=50,
+            c='orange',
+            edgecolor='black',
+            marker='o',
+            label='cluster 2')
+
+plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1],
+            s=250, marker='*', c='red', label='centroids')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+#plt.savefig('images/11_05.png', dpi=300)
+plt.show()
+
+
