bd8858613103cc80578d5d32c6ca1d27adee4ecf
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-22 13:02:15 +0900

    ch11: attaching dendrogram to heatmap

---
+++ ch11/dendrogram_heatmap.py
@@ -0,0 +1,38 @@
+# ## Attaching dendrograms to a heat map
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+from scipy.cluster.hierarchy import linkage
+from scipy.cluster.hierarchy import dendrogram
+
+# 2019.10.22 add
+from random_sample_5x3 import df, labels
+row_clusters = linkage(pdist(df, metric='euclidean'), method='complete')
+
+
+# plot row dendrogram
+fig = plt.figure(figsize=(8, 8), facecolor='white')
+axd = fig.add_axes([0.09, 0.1, 0.2, 0.6])
+
+# note: for matplotlib < v1.5.1, please use orientation='right'
+row_dendr = dendrogram(row_clusters, orientation='left')
+
+# reorder data with respect to clustering
+df_rowclust = df.iloc[row_dendr['leaves'][::-1]]
+
+axd.set_xticks([])
+axd.set_yticks([])
+
+# remove axes spines from dendrogram
+for i in axd.spines.values():
+    i.set_visible(False)
+
+# plot heatmap
+axm = fig.add_axes([0.23, 0.1, 0.6, 0.6])  # x-pos, y-pos, width, height
+cax = axm.matshow(df_rowclust, interpolation='nearest', cmap='hot_r')
+fig.colorbar(cax)
+axm.set_xticklabels([''] + list(df_rowclust.columns))
+axm.set_yticklabels([''] + list(df_rowclust.index))
+
+#plt.savefig('images/11_12.png', dpi=300)
+plt.show()
+
