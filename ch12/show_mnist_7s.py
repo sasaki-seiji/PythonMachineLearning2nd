13a292dca4a204d0aca43551d7b94d866ddbfcbc
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-27 11:40:45 +0900

    ch12: show MNIST images

---
+++ ch12/show_mnist_7s.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = np.load('mnist_scaled.npz')
+X_train, y_train, X_test, y_test = [mnist[f] for f in ['X_train', 'y_train', 
+                                    'X_test', 'y_test']]
+
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(25):
+    img = X_train[y_train == 7][i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+# plt.savefig('images/12_6.png', dpi=300)
+plt.show()
