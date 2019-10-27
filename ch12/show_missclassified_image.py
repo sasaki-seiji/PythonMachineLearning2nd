42a79ab5f8b031f5889cdb3991e5829d0cc70d9b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-10-27 19:21:35 +0900

    ch12: show miss-classified images

---
+++ ch12/show_missclassified_image.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 2019.10.27 add
+mnist = np.load('mnist_scaled.npz')
+X_train, y_train, X_test, y_test = [mnist[f] for f in ['X_train', 'y_train', 
+                                    'X_test', 'y_test']]
+
+# 2019.10.27 add
+import pickle
+nn = pickle.load(open('neuralnet_mnist.pkl', 'rb'))
+
+y_test_pred = nn.predict(X_test)
+acc = (np.sum(y_test == y_test_pred)
+       .astype(np.float) / X_test.shape[0])
+
+print('Test accuracy: %.2f%%' % (acc * 100))
+
+
+
+
+miscl_img = X_test[y_test != y_test_pred][:25]
+correct_lab = y_test[y_test != y_test_pred][:25]
+miscl_lab = y_test_pred[y_test != y_test_pred][:25]
+
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(25):
+    img = miscl_img[i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    ax[i].set_title('%d) t: %d p: %d' % (i+1, correct_lab[i], miscl_lab[i]))
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+#plt.savefig('images/12_09.png', dpi=300)
+plt.show()
