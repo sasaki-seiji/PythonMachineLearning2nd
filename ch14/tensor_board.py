7db5b23783b7e88af1bdddb705686d330c292161
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-11-27 16:32:36 +0900

    ch14: tensorboard sample

---
+++ ch14/tensor_board.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+from helper_functions import build_generator, build_classifier
+###########################
+##  Building the graph   ##
+###########################
+
+batch_size=64
+g = tf.Graph()
+
+with g.as_default():
+    tf_X = tf.compat.v1.placeholder(shape=(batch_size, 100), 
+                          dtype=tf.float32,
+                          name='tf_X')
+    
+    ## build the generator
+    with tf.compat.v1.variable_scope('generator'):
+        gen_out1 = build_generator(data=tf_X, 
+                                   n_hidden=50)
+    
+    ## build the classifier
+    with tf.compat.v1.variable_scope('classifier') as scope:
+        ## classifier for the original data:
+        cls_out1 = build_classifier(data=tf_X, 
+                                    labels=tf.ones(
+                                        shape=batch_size))
+        
+        ## reuse the classifier for generated data
+        scope.reuse_variables()
+        cls_out2 = build_classifier(data=gen_out1[1],
+                                    labels=tf.zeros(
+                                        shape=batch_size))
+
+        
+with tf.compat.v1.Session(graph = g) as sess:
+    sess.run(tf.compat.v1.global_variables_initializer())
+    
+    file_writer = tf.compat.v1.summary.FileWriter(logdir='logs/', graph=g)
+
