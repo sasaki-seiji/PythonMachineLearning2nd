87a878b5f338af814b55c5cb620661db991b761c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-04 19:52:59 +0900

    ch15: load mnist data

---
+++ ch15/load_mnist.py
@@ -0,0 +1,41 @@
+import os
+import struct
+import numpy as np
+
+
+def load_mnist(path, kind='train'):
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels.idx1-ubyte'
+                                % kind)
+    images_path = os.path.join(path,
+                               '%s-images.idx3-ubyte'
+                               % kind)
+
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II',
+                                 lbpath.read(8))
+        labels = np.fromfile(lbpath,
+                             dtype=np.uint8)
+
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack(">IIII",
+                                               imgpath.read(16))
+        images = np.fromfile(imgpath,
+                             dtype=np.uint8).reshape(len(labels), 784)
+
+    return images, labels
+
+
+X_data, y_data = load_mnist('mnist', kind='train')
+print('Rows: %d,  Columns: %d' % (X_data.shape[0], X_data.shape[1]))
+X_test, y_test = load_mnist('mnist', kind='t10k')
+print('Rows: %d,  Columns: %d' % (X_test.shape[0], X_test.shape[1]))
+
+X_train, y_train = X_data[:50000,:], y_data[:50000]
+X_valid, y_valid = X_data[50000:,:], y_data[50000:]
+
+print('Training:   ', X_train.shape, y_train.shape)
+print('Validation: ', X_valid.shape, y_valid.shape)
+print('Test Set:   ', X_test.shape, y_test.shape)
+
