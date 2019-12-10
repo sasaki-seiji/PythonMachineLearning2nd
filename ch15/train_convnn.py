cff5ff9f83a6b103ab3da133e08622303388e29e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-10 21:36:31 +0900

    ch15: CNN implemented by tensorflow layers API

---
+++ ch15/train_convnn.py
@@ -0,0 +1,12 @@
+from std_mnist import X_train_centered, y_train, X_valid_centered, y_valid
+from convnn import ConvNN
+
+cnn = ConvNN(random_seed=123)
+#cnn = ConvNN(epochs=2, random_seed=123)
+
+cnn.train(training_set=(X_train_centered, y_train), 
+          validation_set=(X_valid_centered, y_valid))
+
+# 2019.12.10 del: ValueError
+#cnn.save(epoch=20)
+#cnn.save(epoch=2)
