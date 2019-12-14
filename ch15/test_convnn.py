3e192c9ddcd4f16fc99a46f099340e574ff30566
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-14 19:48:41 +0900

    ch15: test CNN built by tensorflow.layers API

--- ch15/test_convnn.py
+++ ch15/test_convnn.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 from std_mnist import X_test_centered, y_test
-from train_convnn import cnn
+from convnn import ConvNN
+
+cnn = ConvNN(random_seed=123)
+
+cnn.load(epoch=20, path='./tflayers-model/')
 
 print(cnn.predict(X_test_centered[:10,:]))
 
