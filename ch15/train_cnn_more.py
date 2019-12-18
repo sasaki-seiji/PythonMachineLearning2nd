3cef04a3605db0390c1293b02498955d8f6cc78b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-18 18:17:12 +0900

    ch15: train_cnn_more.py: tf.disable_v2_behavior()

--- ch15/train_cnn_more.py
+++ ch15/train_cnn_more.py
@@ -2,8 +2,11 @@
 ## without re-initializing :: initialize=False
 ## create a new session 
 ## and restore the model
-import tensorflow as tf
 import numpy as np
+# 2019.12.18 change
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 from build_cnn import build_cnn
 from cnn_utils import train, save, load, predict
@@ -18,14 +21,14 @@ np.random.seed(random_seed)
 ## and build the model
 g2 = tf.Graph()
 with g2.as_default():
-    tf.compat.v1.set_random_seed(random_seed)
+    tf.set_random_seed(random_seed)
     ## build the graph
     build_cnn()
 
     ## saver:
-    saver = tf.compat.v1.train.Saver()
+    saver = tf.train.Saver()
 
-with tf.compat.v1.Session(graph=g2) as sess:
+with tf.Session(graph=g2) as sess:
     load(saver, sess, 
          epoch=20, path='./model/')
     
