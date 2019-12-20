59f29543217ee76d6792b34e70931fd6d102c394
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-21 08:57:17 +0900

    ch16: prepare IMDb dataset for RNN

---
+++ ch16/prepare_imdb.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from word2int_review import mapped_reviews, df
+
+## Define fixed-length sequences:
+## Use the last 200 elements of each sequence
+## if sequence length < 200: left-pad with zeros
+
+sequence_length = 200  ## sequence length (or T in our formulas)
+sequences = np.zeros((len(mapped_reviews), sequence_length), dtype=int)
+for i, row in enumerate(mapped_reviews):
+    review_arr = np.array(row)
+    sequences[i, -len(row):] = review_arr[-sequence_length:]
+
+X_train = sequences[:25000, :]
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = sequences[25000:, :]
+y_test = df.loc[25000:, 'sentiment'].values
