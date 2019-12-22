0fb48bb2c8210d79373093b0b640b9706cc6bd22
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-22 18:17:17 +0900

    ch16: separate train/test for IBDb

---
+++ ch16/test_imdb.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from prepare_imdb import word_to_int, sequence_length, X_test, y_test
+from sentiment_rnn import SentimentRNN
+
+## Train:
+
+n_words = max(list(word_to_int.values())) + 1
+
+rnn = SentimentRNN(n_words=n_words, 
+                   seq_len=sequence_length,
+                   embed_size=256, 
+                   lstm_size=128, 
+                   num_layers=1, 
+                   batch_size=100, 
+                   learning_rate=0.001)
+
+
+## Test: 
+preds = rnn.predict(X_test)
+y_true = y_test[:len(preds)]
+print('Test Acc.: %.3f' % (
+      np.sum(preds == y_true) / len(y_true)))
+
+
+
+## Get probabilities:
+proba = rnn.predict(X_test, return_proba=True)
+print('Test proba.: ', proba)
