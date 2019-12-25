9466bc6854a396fcaea9583550633edcaaa4343e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-25 14:57:01 +0900

    ch16: utilities of CharRNN

--- ch16/char_rnn_utiles.py
+++ ch16/char_rnn_utiles.py
@@ -27,3 +27,20 @@ print(train_x.shape)
 print(train_x[0, :10])
 print(train_y[0, :10])
 print(''.join(int2char[i] for i in train_x[0, :50]))
+
+
+np.random.seed(123)
+
+def create_batch_generator(data_x, data_y, num_steps):
+    batch_size, tot_batch_length = data_x.shape    
+    num_batches = int(tot_batch_length/num_steps)
+    for b in range(num_batches):
+        yield (data_x[:, b*num_steps: (b+1)*num_steps], 
+               data_y[:, b*num_steps: (b+1)*num_steps])
+        
+bgen = create_batch_generator(train_x[:,:100], train_y[:,:100], 15)
+for b in bgen:
+    print(b[0].shape, b[1].shape, end='  ')
+    print(''.join(int2char[i] for i in b[0][0,:]).replace('\n', '*'), '    ',
+          ''.join(int2char[i] for i in b[1][0,:]).replace('\n', '*'))
+
