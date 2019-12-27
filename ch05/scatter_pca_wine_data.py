027ec024f3396b71c8796a3036a0d0c80b4747f1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-28 07:27:14 +0900

    ch05: scatter pca of wine dataset

---
+++ ch05/scatter_pca_wine_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from std_wine_data import X_train_std, y_train
+from pca_wine_data import w
+
+
+X_train_pca = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0], 
+                X_train_pca[y_train == l, 1], 
+                c=c, label=l, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_03.png', dpi=300)
+plt.show()
