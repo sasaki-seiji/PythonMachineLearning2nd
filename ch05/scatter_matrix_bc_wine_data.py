44da648661eb2fad86f65d9d5f797f1f33d27284
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-30 07:09:46 +0900

    ch05: between-class scatter matrix of wine dataset

---
+++ ch05/scatter_matrix_bc_wine_data.py
@@ -0,0 +1,17 @@
+# Compute the between-class scatter matrix:
+import numpy as np
+
+from std_wine_data import X_train, X_train_std, y_train
+from class_means_wine_data import mean_vecs
+
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13  # number of features
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train[y_train == i + 1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)  # make column vector
+    mean_overall = mean_overall.reshape(d, 1)  # make column vector
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+
