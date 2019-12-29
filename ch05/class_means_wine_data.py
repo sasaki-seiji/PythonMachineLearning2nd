e5add6e44806ff17f5995b36ddb67421c3c6c48e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-30 06:47:20 +0900

    ch05: mean vectors of each class in wine dataset

---
+++ ch05/class_means_wine_data.py
@@ -0,0 +1,12 @@
+# Calculate the mean vectors for each class:
+import numpy as np
+
+from std_wine_data import X_train_std, y_train
+
+np.set_printoptions(precision=4)
+
+mean_vecs = []
+for label in range(1, 4):
+    mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label - 1]))
+
