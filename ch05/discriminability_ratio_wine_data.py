632f320c7f41d32876236555e1eb34b439c5f568
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-30 10:02:27 +0900

    ch05: linear discriminability ratio of wine dataset

---
+++ ch05/discriminability_ratio_wine_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from lda_eigenvalues_wine_data import eigen_vals
+
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+
+plt.bar(range(1, 14), discr, alpha=0.5, align='center',
+        label='individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid',
+         label='cumulative "discriminability"')
+plt.ylabel('"discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.tight_layout()
+# plt.savefig('images/05_07.png', dpi=300)
+plt.show()
