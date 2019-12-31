8ce326988016aaaa97ee6e9c1197e3f41128064d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-31 10:17:40 +0900

    ch05: plot circles

---
+++ ch05/plot_circles.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+# plt.savefig('images/05_15.png', dpi=300)
+plt.show()
+
