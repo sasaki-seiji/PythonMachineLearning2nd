1527e379c2c3724dab4b29b576135bded4cc2a4b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-12-31 09:14:57 +0900

    ch05: plot half moons

---
+++ ch05/plot_moons.py
@@ -0,0 +1,15 @@
+# ### Example 1: Separating half-moon shapes
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+
+
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+# plt.savefig('images/05_12.png', dpi=300)
+plt.show()
