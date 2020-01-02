4fd326920da98f27b8965be424c925c3466f4553
sasaki <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-02 16:49:47 +0900

    ch06: stratified kfold validation of pipelined estimator for breast cancer wisconsin dataset

---
+++ ch06/stratified_kfold_bcw.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+
+from pipeline_breast_cancer import X_train, y_train, pipe_lr
+
+kfold = StratifiedKFold(n_splits=10,
+                        random_state=1).split(X_train, y_train)
+
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k+1,
+          np.bincount(y_train[train]), score))
+    
+print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
