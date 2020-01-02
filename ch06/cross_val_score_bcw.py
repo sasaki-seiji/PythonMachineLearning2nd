2abb85d2ccaa9f7fcf1d24fdcd727babac4b5cc0
sasaki <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-02 16:56:46 +0900

    ch16: cross_val_score() for breast cancer wisconsin

---
+++ ch06/cross_val_score_bcw.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.model_selection import cross_val_score
+
+from pipeline_breast_cancer import pipe_lr, X_train, y_train
+
+scores = cross_val_score(estimator=pipe_lr,
+                         X=X_train,
+                         y=y_train,
+                         cv=10,
+                         n_jobs=1)
+print('CV accuracy scores: %s' % scores)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
