b8c5bce87adc74391cdbceb13b8de77c37f6e341
sasaki <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-03 08:41:58 +0900

    ch06: validation curve of Breast Cancer Wisconsin

---
+++ ch06/validation_curve_bcw.py
@@ -0,0 +1,56 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import validation_curve
+
+from prepare_breast_cancer import X_train, y_train
+
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        LogisticRegression(penalty='l2', random_state=1))
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+                estimator=pipe_lr, 
+                X=X_train, 
+                y=y_train, 
+                param_name='logisticregression__C', 
+                param_range=param_range,
+                cv=10)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, 
+         color='blue', marker='o', 
+         markersize=5, label='training accuracy')
+
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+
+plt.plot(param_range, test_mean, 
+         color='green', linestyle='--', 
+         marker='s', markersize=5, 
+         label='validation accuracy')
+
+plt.fill_between(param_range, 
+                 test_mean + test_std,
+                 test_mean - test_std, 
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+# plt.savefig('images/06_06.png', dpi=300)
+plt.show()
+
+
