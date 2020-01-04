99923a0b44b00306eceec591fefd4392f167c7fd
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-04 13:09:28 +0900

    ch06: matshow confusion matrix of breast cancer wisconsin

---
+++ ch06/matshow_confusion_bcw.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+from confusion_matrix_bcw import confmat
+
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+
+plt.tight_layout()
+#plt.savefig('images/06_09.png', dpi=300)
+plt.show()
+
