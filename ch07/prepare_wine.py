8060d67c365285e834c7cd6f5a8dd9d745a7b92f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-07 15:44:15 +0900

    ch07: print wine dataset shape

--- ch07/prepare_wine.py
+++ ch07/prepare_wine.py
@@ -42,4 +42,5 @@ X_train, X_test, y_train, y_test =            train_test_split(X, y,
                              stratify=y)
 
 # 2020.01.07 add
-print('X_train[0]: ', X_train[0], ', y_train[0]: ', y_train[0])
+print('X_train.shape:', X_train.shape)
+print('y_train.shape:', y_train.shape)
