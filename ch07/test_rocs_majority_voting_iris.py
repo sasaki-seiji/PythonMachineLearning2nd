fe947c67594afa952ae0162c4039546e867a061c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-07 11:10:19 +0900

    ch07: evaluate ROCs including majority vote classifier for iris dataset

---
+++ ch07/test_rocs_majority_voting_iris.py
@@ -0,0 +1,39 @@
+# # Evaluating and tuning the ensemble classifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+from prepare_iris import X_train, y_train, X_test, y_test
+from rocs_majority_voting_iris import all_clf, clf_labels
+
+
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+
+    # assuming the label of the positive class is 1
+    y_pred = clf.fit(X_train,
+                     y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test,
+                                     y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr,
+             color=clr,
+             linestyle=ls,
+             label='%s (auc = %0.2f)' % (label, roc_auc))
+
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1],
+         linestyle='--',
+         color='gray',
+         linewidth=2)
+
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid(alpha=0.5)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+
+
+#plt.savefig('images/07_04', dpi=300)
+plt.show()
