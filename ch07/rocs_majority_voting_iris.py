55db1c9c55cef8a854a822adcc74b45e3715fc75
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-07 10:29:17 +0900

    ch07: ROCs including majority vote classifier for iris dataset

---
+++ ch07/rocs_majority_voting_iris.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import cross_val_score
+
+from rocs_iris import pipe1, clf2, pipe3, clf_labels, X_train, y_train
+from majority_vote_classifier import MajorityVoteClassifier
+
+# Majority Rule (hard) Voting
+
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
+    print("ROC AUC: %0.2f (+/- %0.2f) [%s]"
+          % (scores.mean(), scores.std(), label))
+
