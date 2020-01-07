70e5502eac19651d47e8761b54c4306b4fb6b75a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-07 15:53:20 +0900

    ch07: compare accuracy bagging-tree and raw-tree for wine dataset

---
+++ ch07/accuracy_bagging_500trees_wine.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import accuracy_score
+
+from prepare_wine import X_train, y_train, X_test, y_test
+from bagging_500trees import tree, bag
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f'
+      % (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+
+bag_train = accuracy_score(y_train, y_train_pred) 
+bag_test = accuracy_score(y_test, y_test_pred) 
+print('Bagging train/test accuracies %.3f/%.3f'
+      % (bag_train, bag_test))
+
