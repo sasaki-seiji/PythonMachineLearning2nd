e3f7f2c6c009c77f6a5d772d4002f6b732ba38e6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2020-01-08 18:57:38 +0900

    ch07: accuracy score of AdaBoost of 500 trees for wine dataset

---
+++ ch07/accuracy_adaboost_wine.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import accuracy_score
+
+from prepare_wine import X_train, y_train, X_test, y_test
+from adaboost_500trees import tree, ada
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f'
+      % (tree_train, tree_test))
+
+ada = ada.fit(X_train, y_train)
+y_train_pred = ada.predict(X_train)
+y_test_pred = ada.predict(X_test)
+
+ada_train = accuracy_score(y_train, y_train_pred) 
+ada_test = accuracy_score(y_test, y_test_pred) 
+print('AdaBoost train/test accuracies %.3f/%.3f'
+      % (ada_train, ada_test))
---
+++ ch07/adaboost_500trees.py
@@ -0,0 +1,12 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+
+tree = DecisionTreeClassifier(criterion='entropy', 
+                              max_depth=1,
+                              random_state=1)
+
+ada = AdaBoostClassifier(base_estimator=tree,
+                         n_estimators=500, 
+                         learning_rate=0.1,
+                         random_state=1)
